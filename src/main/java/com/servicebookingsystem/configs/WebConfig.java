2bb6b34f8834e04df46dff2e90c4b24ad7fce7d3
proud <proudndlovu519@gmail.com>
2025-10-14 16:45:30 +0200

    service-booking-system-backend

--- src/main/java/com/servicebookingsystem/configs/WebConfig.java
+++ src/main/java/com/servicebookingsystem/configs/WebConfig.java
@@ -6,13 +6,40 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-               // .allowedOrigins("http://localhost:4200")
-                .allowedOrigins("https://service-booking-system-frontend.vercel.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins(
+                        "https://service-booking-system-frontend.vercel.app",
+                        "http://localhost:4200"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .exposedHeaders("Authorization", "Content-Type")
+                .allowCredentials(true)
+                .maxAge(3600); // 1 hour
     }
 }
+
+
+
+
+//package com.servicebookingsystem.configs;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//               // .allowedOrigins("http://localhost:4200")
+//                .allowedOrigins("https://service-booking-system-frontend.vercel.app")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
+//}
